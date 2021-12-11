de5613136ea1b7f68ea47c03738ee93ad692dabf
Shuaipeng Wang <Antiver@foxmail.com>
2021-12-11 13:21:35 +0800

    减少使用dropout

--- models/RevealNet.py
+++ models/RevealNet.py
@@ -48,7 +48,6 @@ class RevealNet(nn.Module):
         # input is (3) x 256 x 256
         self.main = nn.Sequential(
             nn.Conv2d(nc, nhf, 3, 1, 1),
-            nn.Dropout(0.5),
             nn.BatchNorm2d(nhf),
             nn.ReLU(True),
 
@@ -62,7 +61,6 @@ class RevealNet(nn.Module):
             # nn.ReLU(True),
 
             nn.Conv2d(nhf * 2, nhf * 2, 3, 1, 1),
-            nn.Dropout(0.5),
             nn.BatchNorm2d(nhf*2),
             nn.ReLU(True),
 
@@ -72,7 +70,6 @@ class RevealNet(nn.Module):
             nn.ReLU(True),
 
             nn.Conv2d(nhf, nc, 3, 1, 1),
-            nn.Dropout(0.5),
             output_function()
         )
 
